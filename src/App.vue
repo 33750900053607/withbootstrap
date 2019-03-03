c31a24339e83fde24209f49581451cdd90d0bc5c
33750900053607 <47789111+33750900053607@users.noreply.github.com>
2019-03-03 17:40:14 +0900

    test change
    ### $$$

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
       <router-link to="/">Home</router-link> |***|
       <router-link to="/about">About</router-link>
     </div>
-    ###
+    <div>###</div>
     <router-view/>
-    $$$$
+    <div>$$$</div>
   </div>
 </template>
 
